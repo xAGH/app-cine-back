61d318cf8090ec01b1fea338f2aec4e701ecc463
briancastro-bc <camilitooso77@gmail.com>
2021-10-07 13:26:49 -0500

    Add: CORS requests. BC

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from src.routes import routes, invoicing_routes
 
 class Aplication():
@@ -8,6 +9,17 @@ class Aplication():
     def __init__(self):
         self.app = Flask(__name__)
         self.__register_routes()
+        self.__settings()
+    
+    def __settings(self):
+        try:
+            CORS(self.app, resources={
+                r"/*": {
+                    "origins": ["http://localhost:4200", "*"]
+                }
+            }, supports_credentials=True)
+        except Exception:
+            pass
 
     def __register_routes(self):
         #self.app.add_url_rule(routes["index"], view_func=routes["index_controller"])
